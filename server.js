f3e683a61de0d38cd1e45a7676921956c06032fe
Vitaliy Kurochkin <vitaliy@kurochk.in>
2018-04-15 11:34:59 +0300

    Add page

--- server.js
+++ server.js
@@ -39,6 +39,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    Title: 'Project page',
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Something bad.'
